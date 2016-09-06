bf212c8045b0a75570da0418d760e33f53869e11
August von Trapp and Brian Grant <august+brian@>
2016-09-06 10:56:33 -0700

    add animal restful routes and views and regions show and index

---
+++ app/controllers/regions_controller.rb
@@ -0,0 +1,13 @@
+class RegionsController < ApplicationController
+
+  def index
+    @regions = Region.all
+    render :index
+  end
+
+  def show
+    @region = Region.find(params[:id])
+    render :show
+  end
+
+end
